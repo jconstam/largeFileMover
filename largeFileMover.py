1220d86adac23606b4cb85d627e0d7eeae249537
Jason Constam <callmebob@gmail.com>
2019-07-19 22:36:51 +0200

    Moved file data to new class in module

--- largeFileMover.py
+++ largeFileMover.py
@@ -2,22 +2,14 @@ import os
 import six
 import sys
 
-def getLargestFiles( path, count ):
-    fileList = []
-    for root, dirs, files in os.walk( os.path.abspath( path ) ):
-        for file in files:
-            fileName = os.path.abspath( os.path.join( root, file ) )
-            if not '/.' in fileName:
-                fileSize = os.path.getsize( fileName )
-                fileList.append( ( fileName, fileSize ) )
-
-    fileList.sort( key=lambda filename: filename[ 1 ], reverse=True )
-
-    return fileList[:count]
+from modules.FileData import FileData as FileData
 
 if __name__ == '__main__':
     six.print_( 'Python version: {0}'.format( sys.version ) )
 
-    six.print_( getLargestFiles( './', 10 ) )
+    data = FileData( )
+    data.findFiles( './' )
+    data.sort( )
+    six.print_( data.fileList )
 
-exit( 0 )
+    exit( 0 )
---
+++ modules/FileData.py
@@ -0,0 +1,42 @@
+import os
+import six
+import sys
+import hashlib
+
+class FileInfo:
+    def __init__( self, filePath, fileSize = '', fileMD5 = None ):
+        self.filePath = filePath
+
+        if fileSize:
+            self.fileSize = fileSize
+        else:
+            self.fileSize = os.path.getsize( filePath )
+
+        if fileMD5:
+            self.fileMD5 = fileMD5
+        else:
+            self.fileMD5 = hashlib.md5( open( filePath, 'rb' ).read( ) )
+
+    def __repr__( self ):
+        return '{0} - {1} - {2}'.format( self.filePath, self.fileSize, self.fileMD5.hexdigest( ) )
+
+class FileData:
+    def __init__( self ):
+        self.fileList = []
+
+    def findFiles( self, path ):
+        fileList = []
+        for root, dirs, files in os.walk( os.path.abspath( path ) ):
+            for file in files:
+                filePath = os.path.abspath( os.path.join( root, file ) )
+                if not '/.' in filePath:
+                    self.fileList.append( FileInfo( filePath ) )
+
+    def sort( self ):
+        self.fileList.sort( key=lambda file:file.fileSize, reverse=True )
+
+    def __repr__( self ):
+        output = ''
+        for file in self.fileList:
+            output += file
+        return output
