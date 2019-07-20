d13b4058c8ea8d59f0fcd63e5d8117bbdad562b3
Jason Constam <callmebob@gmail.com>
2019-07-20 12:34:25 +0200

    Added code to create json file

--- largeFileMover.py
+++ largeFileMover.py
@@ -5,11 +5,9 @@ import sys
 from modules.FileData import FileData as FileData
 
 if __name__ == '__main__':
-    six.print_( 'Python version: {0}'.format( sys.version ) )
-
     data = FileData( )
     data.findFiles( './' )
     data.sort( )
-    six.print_( data.fileList )
+    data.writeFile( './test.json' )
 
     exit( 0 )
