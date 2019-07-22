829590d2879b856380ddef5ae180f94ad0db3023
Jason Constam <callmebob@gmail.com>
2019-07-22 19:22:07 +0200

    Added ability to specify root path

--- largeFileMover.py
+++ largeFileMover.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python
+
 import os
+import argparse
 
 from modules.FileData import FileData as FileData
+
+def parseArgs( ):
+    parser = argparse.ArgumentParser( )
+    parser.add_argument( '-r', '--rootPath', required=True, help='Root path to search for files' )
+    return parser.parse_args( )
 
 if __name__ == '__main__':
     if not os.path.exists( './testing' ):
         os.makedirs( './testing' )
+    
+    args = parseArgs( )
 
     data = FileData( )
-    data.findFiles( './' )
+    data.findFiles( args.rootPath )
     data.sort( )
     data.writeFile( './testing/test.json' )
 
